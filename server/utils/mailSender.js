31d6dbaf4a3cb628f6854ff40696e7dc82b67533
Navneet Pratap <pratap102718@gmail.com>
2026-01-23 22:14:27 +0530

    finall all setup

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,27 +2,29 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
+    // Transporter configuration for Gmail
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST, 
-      port: Number (process.env.MAIL_PORT), 
-      secure: false,
+      port: Number(process.env.MAIL_PORT), 
+      secure: true, 
       auth: {
         user: process.env.MAIL_USER, 
         pass: process.env.MAIL_PASS, 
       },
     });
 
+    // Sending the email
     const info = await transporter.sendMail({
-      from: `"StudyNotion" <${process.env.MAIL_FROM}>`,
-      to: email,
-      subject: title,
-      html: body,
+      from: `"StudyNotion" <${process.env.MAIL_USER}>`, // Sender address
+      to: `${email}`, 
+      subject: `${title}`, 
+      html: `${body}`, 
     });
 
-    console.log("BREVO MAIL SENT ", info.response);
+    console.log("Email info: ", info);
     return info;
   } catch (error) {
-    console.error(" BREVO MAIL ERROR ", error);
+    console.log("Error occurred while sending email: ", error.message);
     throw error;
   }
 };
