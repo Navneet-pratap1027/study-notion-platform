7b11ec0434c70a86222ece063cb1ab0811ffff27
Navneet Pratap <pratap102718@gmail.com>
2026-01-23 20:35:55 +0530

    fix mail sender

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,7 +4,7 @@ const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST, 
-      port: process.env.MAIL_PORT, 
+      port: Number (process.env.MAIL_PORT), 
       secure: false,
       auth: {
         user: process.env.MAIL_USER, 
